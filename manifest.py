f44b9764fbd96020942bf8a8bddefe73d5f18317
UxioMendezPRO <uxio.mendez@hotmail.com>
2024-06-18 20:40:44 +0200

    Elementos básicos

---
+++ manifest.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Uxio Mendez Pazos <uxio.mendez@hotmail.com>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "sports_club",
+    "summary": "Custom module",
+    "version": "16.0.0.0.0",
+    "category": "Sports",
+    "website": "https://github.com/UxioMendezPRO/sports_club",
+    "author": "Uxio Mendez Pazos",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": ["base"],
+    'application': True,
+    "data": ["views/res_partner.xml"],
+
+}
---
+++ models/res_partner.py
@@ -0,0 +1,10 @@
+# Copyright 2024 Uxio Mendez Pazos <uxio.mendez@hotmail.com>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from odoo import fields, models
+
+class ResPartner(models.Model):
+    _name = 'res.partner'
+    _inherit = ['res.partner']
+
+    
