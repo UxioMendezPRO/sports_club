ebdf89cd3b1c1c15555619459bad82dcb6dce386
UxioMendezPRO <uxio.mendez@hotmail.com>
2024-06-26 20:32:55 +0200

    [ADD]sports_club_menus: vista coach

--- models/res_partner.py
+++ models/res_partner.py
@@ -3,13 +3,13 @@
 from odoo import fields, models, api
 
 
-class ResPartner(models.Model):
+class ResPartner(models.Model):  # pq falla estooo????
     _inherit = "res.partner"
 
-
+    _name = "res.partner"
+    _description = "res.partner inheritance"
 
     id = fields.Char(string="ID")
     associate_number = fields.Integer(string="Associate number")
     registration_date = fields.Date(string="Registration date")
     withdrawal_date = fields.Date(string="Withdrawal date")
-    
